ea2619d31e148e65bc483d5376bbbe93f8f10ffd
ashikurbd71 <ashikurovi2003@gmail.com>
2023-11-25 04:41:48 +0600

    mongodb setup done

--- index.js
+++ index.js
@@ -2,12 +2,54 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000
 
 // midleware
 
 app.use(cors())
 app.use(express.json())
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mtnypra.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+    // databage
+    const userCampColaction = client.db("MedicalCampDB").collection("Users");
+    const addCampColaction = client.db("MedicalCampDB").collection("AddCamp");
+    const managesCampColaction = client.db("MedicalCampDB").collection("ManagesCamp");
+    const registerCampColaction = client.db("MedicalCampDB").collection("RegisterCamp");
+    
+
+
+   
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
 
 
 app.get('/', (req, res) => {
