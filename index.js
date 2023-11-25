273bfad0fcd39ad2d6c6d9d0b5dade2bb4f3dab8
ashikurbd71 <ashikurovi2003@gmail.com>
2023-11-25 16:16:01 +0600

    user data post and get

--- index.js
+++ index.js
@@ -5,9 +5,16 @@ require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000
 
+
+
+const corsOptions = {
+  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  credentials: true,
+  optionSuccessStatus: 200,
+}
 // midleware
 
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(express.json())
 
 
@@ -27,15 +34,68 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
 
-    // databage
+    //----------------------------- databagecolection--------------------------------
     const userCampColaction = client.db("MedicalCampDB").collection("Users");
     const addCampColaction = client.db("MedicalCampDB").collection("AddCamp");
     const managesCampColaction = client.db("MedicalCampDB").collection("ManagesCamp");
     const registerCampColaction = client.db("MedicalCampDB").collection("RegisterCamp");
     
 
+    // ----------------------USERS DATA POST------------------------------
 
+     app.put('/users/:email',async(req,res) => {
+    
+      try{
+
+        const email = req.params.email
+        const user = req.body
+        const query = { email: email }
+        const options = { upsert: true }
+        const isExist = await userCampColaction.findOne(query)
+        console.log('User found?----->', isExist)
+        if (isExist) return res.send(isExist)
+        const result = await userCampColaction.updateOne(
+          query,
+          {
+            $set: { ...user, timestamp: Date.now() },
+          },
+          options
+        )
+        res.send(result)
+      }
+
+       catch(err){
+
+        console.log(errr)
+       }
+
+     })
+
+    // -------------------------USER DATA GET-------------------------------
    
+    app.get('/users/email/:email',async(req,res) => {
+
+     try{
+      const email = req.params.email
+      console.log(email)
+      
+        const result = await userCampColaction.findOne({email})
+        console.log(result)
+        res.send(result)
+     }
+     catch(err){
+      console.log(err)
+     }
+       
+     })
+
+
+
+
+
+
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
